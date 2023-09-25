690c6fa65a8b0bd698e600aaf7daaa64523c9f0e
JuanaGonzalez21 <juanita.juana@hotmail.com>
2023-09-24 20:14:45 -0500

    Parte 2

--- react/org/src/components/Colab/Colab.js
+++ react/org/src/components/Colab/Colab.js
@@ -1,19 +1,27 @@
 import React from 'react'
 import "./Colab.css"
+import { AiFillCloseCircle } from "react-icons/ai"
 
 function Colab(props) {
 
-  const {nombre, puesto, foto, equipo} = props.datos
+  const { nombre, puesto, foto } = props.datos
+  const { colorPrimario, eliminarColaborador } = props
+
+  const colorEncabezado =  {
+    backgroundColor: colorPrimario
+  }
+
   return (
     <div className='colaborador'>
-        <div className='encabezado'>
-          <img src={foto} alt={nombre}></img>
+      <AiFillCloseCircle onClick={eliminarColaborador} className='eliminar'/>
+      <div className='encabezado' style={colorEncabezado}>
+        <img src={foto} alt={nombre}></img>
 
-        </div>
-        <div className='info'>
-            <h4>{nombre}</h4>
-            <h5>{puesto}</h5>
-        </div>
+      </div>
+      <div className='info'>
+        <h4>{nombre}</h4>
+        <h5>{puesto}</h5>
+      </div>
     </div>
   )
 }
